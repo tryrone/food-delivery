ace6548032fb08711130ad8dcbf5df79d76b59c8
tryrone <tegararuvwe@gmail.com>
2023-02-16 22:02:38 +0100

    initial project setup  and assets

---
+++ assets/images/index.js
@@ -0,0 +1,6 @@
+export const twoCups = require("./2_cups.png");
+export const blackCup = require("./black_cup.png");
+export const boxCup = require("./box_cup.png");
+export const kar = require("./kar.png");
+export const lentil = require("./lentil.png");
+export const vegan = require("./vegan.png");
---
+++ assets/svg/index.js
@@ -0,0 +1,27 @@
+import FireSvg from "./fire.svg";
+import LeafSvg from "./leaf.svg";
+import LikeSvg from "./like.svg";
+import LocationPinSvg from "./location_pin.svg";
+import RecycleSvg from "./recycle.svg";
+import SearchSvg from "./search_svg.svg";
+import SettingSvg from "./settings.svg";
+import ShoppingBagSvg from "./shopping_bag.svg";
+import StarSvg from "./star.svg";
+import StockSvg from "./stock.svg";
+import TimeSvg from "./time.svg";
+import UnlikeSvg from "./unlike.svg";
+
+export {
+  FireSvg,
+  LeafSvg,
+  LikeSvg,
+  LocationPinSvg,
+  RecycleSvg,
+  SearchSvg,
+  SettingSvg,
+  ShoppingBagSvg,
+  StarSvg,
+  StockSvg,
+  TimeSvg,
+  UnlikeSvg,
+};
